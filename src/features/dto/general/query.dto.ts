79a095a94a2977560593feb66d38bb453ba3fb3c
andrejrebrov <674511674511aaqqww>
2022-10-16 01:00:32 +0300

    lesson 4 fix 1

--- src/features/dto/general/query.dto.ts
+++ src/features/dto/general/query.dto.ts
@@ -1,13 +1,10 @@
 import { Type } from 'class-transformer';
-import { IsNumber } from 'class-validator';
 
 export class QueryDto {
 	@Type(() => Number)
-	//@IsNumber()
 	pageNumber?: number;
 
 	@Type(() => Number)
-	//@IsNumber()
 	pageSize?: number;
 
 	sortBy?: string;
