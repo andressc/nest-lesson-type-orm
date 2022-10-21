daca8faa8be4e7e2593ffdf598ce50b12146770d
andrejrebrov <674511674511aaqqww>
2022-10-22 02:59:39 +0300

    lesson 7 fix 1

--- src/common/exceptions/UserExistsException.ts
+++ src/common/exceptions/UserExistsException.ts
@@ -1,10 +1,12 @@
-import { ForbiddenException } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common';
 
-export class UserExistsException extends ForbiddenException {
+export class UserExistsException extends BadRequestException {
 	constructor(login: string, email: string) {
-		super({
-			message: `user login = ${login} or email = ${email} already exists`,
-			field: ['email', 'login'],
-		});
+		super([
+			{
+				message: `user login = ${login} or email = ${email} already exists`,
+				field: ['email', 'login'],
+			},
+		]);
 	}
 }
--- src/common/exceptions/confirmCodeBadRequestException.ts
+++ src/common/exceptions/confirmCodeBadRequestException.ts
@@ -2,6 +2,6 @@ import { BadRequestException } from '@nestjs/common';
 
 export class ConfirmCodeBadRequestException extends BadRequestException {
 	constructor() {
-		super([{ message: 'error confirm code', field: 'confirmCode' }]);
+		super([{ message: 'error confirm code', field: 'code' }]);
 	}
 }
