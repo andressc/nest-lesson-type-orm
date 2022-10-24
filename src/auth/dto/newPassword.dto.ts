a5c7e00824d6207dc152f07f971d4e77c192f4ea
andrejrebrov <674511674511aaqqww>
2022-10-25 02:19:40 +0300

    lesson 10 fix 1

--- src/auth/dto/newPassword.dto.ts
+++ src/auth/dto/newPassword.dto.ts
@@ -1,8 +1,9 @@
-import { IsJWT, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsJWT, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class NewPasswordDto {
 	@IsNotEmpty()
 	@IsString()
+	@MinLength(6)
 	@MaxLength(20)
 	newPassword: string;
 	@IsJWT()
