8cd8d50463f21c2c0a864493628db7d79eb9918c
Andrey Rebrov <andressc@mail.ru>
2022-11-12 10:35:25 +0300

    lesson 14 fix 2

--- src/features/comments/application/queries/find-one-comment.handler.ts
+++ src/features/comments/application/queries/find-one-comment.handler.ts
@@ -4,6 +4,8 @@ import { CommentNotFoundException } from '../../../../common/exceptions';
 import { ObjectIdDto } from '../../../../common/dto';
 import { ResponseCommentDto } from '../../dto';
 import { QueryCommentsRepositoryInterface } from '../../interface/query.comments.repository.interface';
+import { UsersRepositoryInterface } from '../../../users/interface/users.repository.interface';
+import { UserModel } from '../../../users/entity/user.schema';
 
 export class FindOneCommentCommand {
 	constructor(public id: string, public currentUserId: ObjectIdDto | null) {}
@@ -11,13 +13,19 @@ export class FindOneCommentCommand {
 
 @QueryHandler(FindOneCommentCommand)
 export class FindOneCommentHandler implements IQueryHandler<FindOneCommentCommand> {
-	constructor(private readonly queryCommentsRepository: QueryCommentsRepositoryInterface) {}
+	constructor(
+		private readonly queryCommentsRepository: QueryCommentsRepositoryInterface,
+		private readonly usersRepository: UsersRepositoryInterface,
+	) {}
 
 	async execute(command: FindOneCommentCommand): Promise<ResponseCommentDto> {
 		const comment: CommentModel | null = await this.queryCommentsRepository.findCommentModel(
 			command.id,
 		);
 		if (!comment) throw new CommentNotFoundException(command.id);
+
+		const user: UserModel = await this.usersRepository.findUserModel(comment.userId);
+		if (user.isBanned) throw new CommentNotFoundException(command.id);
 
 		const likesInfo = this.queryCommentsRepository.countLikes(comment, command.currentUserId);
 
