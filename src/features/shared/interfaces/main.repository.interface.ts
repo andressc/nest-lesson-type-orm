2d7a6f2402597e1be35b262e8ba70a098fddb397
Andrey Rebrov <andressc@mail.ru>
2022-11-27 17:56:32 +0300

    refactoring

--- src/features/shared/interfaces/main.repository.interface.ts
+++ src/features/shared/interfaces/main.repository.interface.ts
@@ -1,7 +1,7 @@
 export interface MainRepositoryInterface<MODEL, TYPE> {
 	create(data: TYPE): Promise<MODEL>;
 	find(id: string): Promise<MODEL | null>;
-	save(blogModel: MODEL): Promise<MODEL>;
-	delete(blogModel: MODEL): Promise<void>;
+	save(model: MODEL): Promise<MODEL>;
+	delete(model: MODEL): Promise<void>;
 	deleteAll(): Promise<void>;
 }
