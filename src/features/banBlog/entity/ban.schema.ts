c0055683381f19847f8eeb74677225ebf2a4c227
Andrey Rebrov <andressc@mail.ru>
2022-11-28 14:28:38 +0300

    Lesson 16 fix 1

---
+++ src/features/banBlog/entity/ban.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type BanModel = Ban & Document;
+
+@Schema()
+export class Ban {
+	@Prop({ required: true })
+	userId: string;
+
+	@Prop({ required: true })
+	login: string;
+
+	@Prop({ required: true })
+	blogId: string;
+
+	@Prop({ default: false })
+	isBanned: boolean;
+
+	@Prop({ default: null })
+	banReason: string;
+
+	@Prop({ default: null })
+	banDate: string;
+
+	@Prop({ required: true })
+	createdAt: string;
+
+	banUnbanUser(isBanned, banReason, banDate): void {
+		this.isBanned = isBanned;
+		this.banReason = !isBanned ? null : banReason;
+		this.banDate = !isBanned ? null : banDate;
+	}
+}
+
+export const BanSchema = SchemaFactory.createForClass(Ban);
+
+BanSchema.methods.banUnbanUser = Ban.prototype.banUnbanUser;
